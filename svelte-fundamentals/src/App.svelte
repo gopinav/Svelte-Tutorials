5e42c8a75f94f14541b1c6324a6956c2d36397df
Vishwas <vishwas.g5@gmail.com>
2021-02-03 21:05:28 +0530

    List Rendering

--- svelte-fundamentals/src/App.svelte
+++ svelte-fundamentals/src/App.svelte
@@ -7,6 +7,12 @@
   const status = 'danger'
   const promoted = true
   const num = 'Hi'
+  const names = ['Bruce', 'Clark', 'Diana']
+  const fullNames = [
+    { first: 'Bruce', last: 'Wayne' },
+    { first: 'Clark', last: 'Kent' },
+    { first: 'Princess', last: 'Diana' }
+  ]
 </script>
 
 <main>
@@ -32,6 +38,14 @@
   {:else}
     <h2>Not a number</h2>
   {/if}
+
+  {#each names as name, index (name)}
+    <h2>{index + 1} {name}</h2>
+  {/each}
+
+  {#each fullNames as name, index (name.first)}
+    <h2>{index + 1} {name.first} {name.last}</h2>
+  {/each}
 </main>
 
 <style>
