cc63709b9a5808c326276ac16c728cade86cc374
Vishwas <vishwas.g5@gmail.com>
2021-02-04 16:25:33 +0530

    Form controls

--- svelte-fundamentals/src/App.svelte
+++ svelte-fundamentals/src/App.svelte
@@ -20,6 +20,16 @@
     console.log(event)
     count += stepSize
   }
+
+  const formValues = {
+    name: '',
+    profileSummary: '',
+    country: '',
+    jobLocation: [],
+    remoteWork: false,
+    skillSet: [],
+    yearsOfExperience: ''
+  }
 </script>
 
 <main>
@@ -56,11 +66,108 @@
 
   <button on:click={e => handleClick(e, 5)}> Count {count} </button>
   <button on:click={e => handleClick(e, 10)}> Count {count} </button>
+
+  <div>
+    <pre>
+      { JSON.stringify(formValues, null, 2) }
+    </pre>
+  </div>
+
+  <form>
+    <div>
+      <label for="name">Name</label>
+      <input type="number" id="name" bind:value={formValues.name} />
+    </div>
+    <div>
+      <label for="profile">Profile Summary</label>
+      <textarea id="profile" bind:value={formValues.profileSummary} />
+    </div>
+    <div>
+      <label for="country">Country</label>
+      <select id="country" bind:value={formValues.country}>
+        <option value="">Select a country</option>
+        <option value="india">India</option>
+        <option value="vietnam">Vietnam</option>
+        <option value="singapore">Singapore</option>
+      </select>
+    </div>
+    <div>
+      <label for="job-location">Job location</label>
+      <select id="job-location" bind:value={formValues.jobLocation} multiple>
+        <option value="india">India</option>
+        <option value="vietnam">Vietnam</option>
+        <option value="singapore">Singapore</option>
+      </select>
+    </div>
+    <div>
+      <input
+        type="checkbox"
+        id="remoteWork"
+        bind:checked={formValues.remoteWork}
+      />
+      <label for="remoteWork">Open to remote work?</label>
+    </div>
+    <div>
+      <label>Skill set</label>
+      <input
+        type="checkbox"
+        id="html"
+        value="html"
+        bind:group={formValues.skillSet}
+      />
+      <label for="html">HTML</label>
+      <input
+        type="checkbox"
+        id="css"
+        value="css"
+        bind:group={formValues.skillSet}
+      />
+      <label for="css">CSS</label>
+      <input
+        type="checkbox"
+        id="javascript"
+        value="javascript"
+        bind:group={formValues.skillSet}
+      />
+      <label for="javascript">JavaScript</label>
+    </div>
+    <div>
+      <label>Years of Experience</label>
+      <input
+        type="radio"
+        id="0-2"
+        value="0-2"
+        bind:group={formValues.yearsOfExperience}
+      />
+      <label for="0-2">0-2</label>
+      <input
+        type="radio"
+        id="3-5"
+        value="3-5"
+        bind:group={formValues.yearsOfExperience}
+      />
+      <label for="3-5">3-5</label>
+      <input
+        type="radio"
+        id="6-10"
+        value="6-10"
+        bind:group={formValues.yearsOfExperience}
+      />
+      <label for="6-10">5-10</label>
+      <input
+        type="radio"
+        id="10+"
+        value="10+"
+        bind:group={formValues.yearsOfExperience}
+      />
+      <label for="10+">10+</label>
+    </div>
+  </form>
 </main>
 
 <style>
   main {
-    text-align: center;
+    /* text-align: center; */
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
@@ -93,5 +200,9 @@
 
   .promoted {
     font-style: italic;
+  }
+
+  input + label {
+    display: inline-flex;
   }
 </style>
